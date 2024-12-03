e005f298050a2fafd669dcb71a91b5c304053fe7
AlexCourivaud <alexandre.courivaud@gmail.com>
2024-12-03 15:21:44 +0100

    creation of entity

---
+++ entities/list_entity.py
@@ -0,0 +1,8 @@
+class ListEntity:
+    def __init__(self, id=None, title=None, mark_as_done=None):
+        self.id = id
+        self.title = title
+        self.mark_as_done = mark_as_done
+
+    def __str__(self):
+        return f"ListEntity(id={self.id}, title={self.title})"
--- views/list_view.py
+++ views/list_view.py
@@ -1,3 +1,2 @@
 #import pyqt6
 from PyQt6.QtWidgets import ( QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QTableWidget, QTableWidgetItem, QMessageBox)
-
